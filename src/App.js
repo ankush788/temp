213af56951579f357c1788b479a7dc4dd4b1b8a9
Ankush Singh <ankushsingh20000003@gmail.com>
2024-05-31 21:46:43 +0530

    deleted design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import Home1 from './Home1';
 import Home2 from './Home2';
-import Design from './Design';
+
 function App() {
   return (
     <div className="App">
       <Home1/>
       <Home2/>
-      <Design/>
     </div>
   );
 }
